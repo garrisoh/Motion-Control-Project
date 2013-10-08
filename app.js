36b6227c187e7e5e1905626badc4c52dee1a9296
Haley Garrison <garrisoh@lafayette.edu>
2013-10-08 14:06:00 -0400

    Added Tape and Mocha from npm, made a box move around (app.js)

--- app.js
+++ app.js
@@ -1,4 +1,3 @@
-;(function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);throw new Error("Cannot find module '"+o+"'")}var f=n[o]={exports:{}};t[o][0].call(f.exports,function(e){var n=t[o][1][e];return s(n?n:e)},f,f.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
 // this exists in index.html
 var canvas = document.getElementById('game');
 var context = canvas.getContext('2d');
@@ -113,5 +112,3 @@ function draw() {
 
 // fire it up!
 startGame();
-},{}]},{},[1])
-;
