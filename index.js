41fde47f7088dc4a8b40e0b476740f7374617b26
Haley Garrison <garrisoh@lafayette.edu>
2013-10-26 18:08:45 -0400

    Fixed percent, added test values

--- index.js
+++ index.js
@@ -23,27 +23,30 @@ var MIN_R = 40;
 controller.on('deviceFrame', function(frame){
     // do things
     var hand = frame.hands[0];
-    var numFingers = hand.fingers.length;
-    if(hand && numFingers >= 2) {
-        var x = hand.palmPosition[0];
-        var y = -1 * hand.palmPosition[2];
-        var z = hand.palmPosition[1];
-        var r = hand.sphereRadius;
-        
-        x = map(x, MAX_X, MIN_X, , );
-        y = map(y, MAX_Y, MIN_Y, , );
-        z = map(z, MAX_Z, MIN_Z, , );
-        r = map(r, MAX_R, MIN_R, , );
-        
-        // pass into Daniel's function
+    if(hand) {
+        var numFingers = hand.fingers.length;
+        if(numFingers >= 2) {
+            var x = hand.palmPosition[0];
+            var y = -1 * hand.palmPosition[2];
+            var z = hand.palmPosition[1];
+            var r = hand.sphereRadius;
+            
+            x = map(x, MAX_X, MIN_X, 40, 0);
+            y = map(y, MAX_Y, MIN_Y, 20, 0);
+            z = map(z, MAX_Z, MIN_Z, 40, 0);
+            r = map(r, MAX_R, MIN_R, 4, 0);
+            
+            // pass into Daniel's function
+            console.log("x: " + x + " y: " + y + " z: " + z + "r: " + r);
+        }
     }
 });
 
 // map a value to new coordinates and constrain to high2 or low2 values if it goes over/under
 function map(value, high1, low1, high2, low2) {
     var percent = (value - low1)/(high1 - low1);
-    if(percent > 100) {
-        percent = 100;
+    if(percent > 1) {
+        percent = 1;
     }
     if(percent < 0) {
         percent = 0;
